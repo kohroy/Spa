2998125582731986618dd59433fe7cf0cba4a638
Roy <kohroy@live.cn>
2020-03-22 14:18:38 +0800

    添加项目文件。

---
+++ Spa.Api/ApplicationBuilderExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace Spa.Api
+{
+    public static class ApplicationBuilderExtensions
+    {
+        public static IApplicationBuilder UseApi(this IApplicationBuilder applicationBuilder)
+        {
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            applicationBuilder.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            applicationBuilder.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                c.RoutePrefix = "ApiDocs";
+            });
+
+            return applicationBuilder;
+        }
+    }
+}
---
+++ Spa.Api/ServiceCollectionExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+
+namespace Spa.Api
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddApi(this IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+            });
+
+            return services;
+        }
+    }
+}
